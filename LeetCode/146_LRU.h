d45930601cad94358bf17c6c01aa7d3ad89c2191
Swa99er <1246318713@qq.com>
2021-04-25 15:27:34 +0800

    update

--- LeetCode/146_LRU.h
+++ LeetCode/146_LRU.h
@@ -14,151 +14,123 @@ class LRUCache {
 
 private:
 
-
-
     struct Node{
         int key;
         int data;
-        time_t expire; /* 使用时间，交给heap管理 */
-
-        Node(int key, int data, time_t expire):key(key), data(data), expire(expire){}
+        struct Node* pre;
+        struct Node* next;
+        Node(int key, int data):key(key),data(data){
+            pre = next = nullptr;
+        }
     };
 
-    std::map<int, Node*> map_;
+
+
+    std::map<int, Node*> map_; /* 哈希表，这里应该是树形实现 */
+
+    struct Node* head_; /* 双向头节点 */
+
+    struct Node* tail_; /* 链表尾节点 */
 
     int capacity; /* 总大小 */
-    int count; /* 当前大小，由heap所维护即可 */
-    Node **heap; /* heap所维护的数组，heap数组从1开始 */
 
+    int count; /* 当前节点大小 */
 
 
 public:
 
-    LRUCache(int capacity) {
+    LRUCache(int capacity):capacity(capacity) {
         this->count = 0;
-        this->capacity = capacity;
-
     }
 
 
 
     void put(int key, int value) {
-
-        /* 已经存在，则进行值替换 */
+        /* 节点已经存在，则更新 */
         if (get(key) != -1){
             map_.find(key)->second->data = value;
             return;
         }
-        /* 新值，进行添加，已满将最久的节点删除 */
-        if ( isFull() ){
-            Node* delNode = pop();
-            map_.erase(delNode->key);
+        /* 未满 */
+        if ( !isFull() ){
+            Node* newNode = new Node(key, value);
+            /* 插入表中 */
+            map_.insert(std::pair<int, Node*>(key, newNode));
+            /* 插入链表 */
+            insert(newNode);
             return;
         }
-        /* 插入新元素 */
-        Node* target = new Node(key, value, time(nullptr));
-        map_.insert(std::pair<int, Node*>(key, target));
-        /* 加入时间堆排序 */
-        heap_insert(target);
+        /* 已满 */
+        pop(); // 删除最少使用节点
+        put(key, value); /* 递归重新调用一次 */
+    }
 
+    int get(int key){
+        auto iter = map_.find(key);
+        if (iter != map_.end() ){
+            update(iter->second);
+            return iter->second->data;
+        }
+        return -1;
     }
 
 
 private:
 
-    int get(int key) {
-        auto iterator = map_.find(key);
-        if (iterator == map_.end()){
-            return -1;
+
+    void update(Node* target){
+        /* 已经是队尾了，不进行操作 */
+        if (target == tail_ )
+            return;
+        /* 队头，那就将其移动到队尾 */
+        if (target == head_ ){
+            /* 将头节点往后移动一个位置 */
+            head_ = target->next;
+            head_->pre = nullptr;
+            /* target插入尾节点 */
+            target->next = nullptr;
+            tail_->next = target;
+            target->pre = tail_;
+            tail_ = target;
+            return;
         }
-        /* 真的存在元素，则返回值并且更新使用时间 */
-        heap_update(iterator->second);
-        return iterator->second->data;
+        /* 队中间 */
+        auto* preNode = target->pre;
+        auto* nxtNode = target->next;
+        preNode->next = nxtNode;
+        nxtNode->pre = preNode;
+        /* 加入到尾部 */
+        target->next = nullptr;
+        target->pre = tail_;
+        tail_->next = target;
+        tail_ = target;
     }
 
 
-    bool isFull(){
-        return count == capacity;
-    }
+    bool isFull(){return count == capacity;}
 
 
-    Node* pop(){
-        Node* ret = heap[1];
-        heap_swap(&heap[1], &heap[count]);
-        -- count;
-        heap_shiftUp(count);
-        return ret;
-    }
-
-
-    /**
-     * 更新事件，将最近使用的元素移动到最后
-     * @param target
-     */
-    void heap_update(Node* target){
-        int idx = heap_find_index(target);
-        heap_swap(&heap[idx], &heap[count]);
-        /* 更新时间 */
-        heap[count]->expire = time(nullptr);
-        /* 将尾巴节点重新排序放到正确地方 */
-        heap_shiftDown(idx);
-    }
-
-
-    int heap_find_index(Node* target){
-
-    }
-
-    /**
-     * 指针交换，最好改成模版类型加以复用
-     * @param left
-     * @param right
-     */
-    void heap_swap(Node **left, Node **right){
-        Node* tmp = *left;
-        *left = *right;
-        *right = tmp;
-    }
-
-    /**
-     * 这里是不需要shiftUp的，毕竟最后添加的使用时间肯定最大
-     * 只有时间最小才需要被删除(绝对时间)
-     * @param target
-     */
-    void heap_insert(Node* target){
-        heap[count + 1] = target;
+    void insert(Node* target){
         ++ count;
-    }
-
-
-    void heap_shiftUp(int target){
-        while (target > 1){
-            if (heap[target]->expire > heap[target/2]->expire)
-                break;
-            heap_swap(&heap[target], &heap[target/2]);
-            target /= 2;
+        if ( !head_ ){
+            head_ = tail_ = target;
+            return;
         }
+        /* 直接加入到尾巴节点即可 */
+        target->pre = tail_;
+        tail_->next = target;
+        tail_ = target;
     }
 
 
-    void heap_shiftDown(int target){
-        int k = target * 2;
-        /* 当target的时间更大时才有必要继续，否则没有必要 */
-        while ( k <= count && heap[target]->expire > heap[k]->expire){
-            if (k + 1 <= count ){
-                /* 找到子节点中最小的那个值 */
-                if (heap[k+1]->expire < heap[k]->expire)
-                    ++ k;
-            }
-            heap_swap(&heap[target], &heap[k]);
-            target = k;
-            k = target * 2;
-        }
+    void pop(){
+        auto* delNode = head_;
+        head_ = head_->next;
+        head_->pre = nullptr; /* 还是将指针置空安全点 */
+        map_.erase(delNode->key); /* 将其从表中删除 */
+        delete delNode;
+        -- count;
     }
-
-
-
-
 
 
 };
--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
-#include "Tree/Tree_test.h"
-#include "Sort/SortTestHelper.h"
-#include "Union/UnionTest.h"
+#include "LeetCode/146_LRU.h"
+#include <iostream>
+
+
+using std::cout;
+using std::endl;
+
 
 int main() {
 
-    // SortMain();
-    int n = 100000;
-    testUF1(n);
-    testUF2(n);
+    auto *lruCache = new LRUCache(2);
+
+    lruCache->put(1, 1); // 缓存是 {1=1}
+    lruCache->put(2, 2); // 缓存是 {1=1, 2=2}
+    cout << lruCache->get(1) << endl;    // 返回 1
+    lruCache->put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+    cout << lruCache->get(2) << endl;    // 返回 -1 (未找到)
+    lruCache->put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+    cout << lruCache->get(1) << endl;    // 返回 -1 (未找到)
+    cout << lruCache->get(3) << endl;    // 返回 3
+    cout << lruCache->get(4) << endl;    // 返回 4
+
 
 }
