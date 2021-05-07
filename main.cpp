96d36bf3af52bbde0af19142cfd98197b1104a8d
Swa99er <1246318713@qq.com>
2021-05-07 18:19:52 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,13 +1,38 @@
 #include "Tree/Tree_test.h"
 #include "Sort/SortTestHelper.h"
-#include "LeetCode/179_largetNumber.h"
+
+
+void you_shang(int **arr, int row, int col){
+
+    for (int i=row; i<10; i--){
+        for (int j=col; j < 10; j++){
+            std::cout << arr[i][j] << "   ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+
 
 int main() {
 
-    Solution s;
-    int x = 4;
-    int y = 54;
-    std::cout << s.cmp(&x, &y) << std::endl;
+
+    int board[10][10] = {};
+    int index = 1;
+    for (int i=0; i<10; i++){
+       for (int j=0; j<10; j++){
+           board[i][j] = index;
+           ++index;
+       }
+    }
+
+//    for (int i=0; i<10; i++){
+//        for (int j=0; j<10; j++){
+//            std::cout << board[i][j] << "    ";
+//        }
+//        std::cout << std::endl;
+//    }
+
 
 
 }
